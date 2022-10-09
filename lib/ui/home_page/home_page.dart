6371efa3408c3b7d255d25a2168a2acb2fbc38b5
Lraty <lraty@outlook.com>
2022-10-09 14:33:30 +0800

    add mobile_scanner & qrCode parsing

--- lib/ui/home_page/home_page.dart
+++ lib/ui/home_page/home_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:get/get.dart';
 
 import 'package:switch_screenshot_transfer/model/page_indicator.dart';
+import 'package:switch_screenshot_transfer/ui/home_page/home_page_logic.dart';
+import 'package:switch_screenshot_transfer/ui/home_page/home_page_state.dart';
 import 'package:switch_screenshot_transfer/ui/navigation_bar/navigation_bar.dart';
 
 class HomePage extends StatefulWidget {
@@ -15,10 +18,78 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // floatingActionButton: FloatingActionButton(onPressed: () => Get.back(),),
+      body: _body(),
       bottomNavigationBar: sNavigationBar(
         pageIndicator: PageIndicator.homePage,
       ),
     );
   }
+}
+
+Widget _body() {
+  final logic = Get.put(HomePageLogic());
+  final state = Get.find<HomePageLogic>().state;
+  return Column(
+    children: [
+      _showScanBox(logic, state),
+      _showStepIndicating(state),
+      _showImgSrc(logic)
+    ],
+  );
+}
+
+Widget _showScanBox(HomePageLogic logic, HomePageState state) {
+  return Expanded(
+    child: MobileScanner(
+        allowDuplicates: false,
+        controller: state.cameraController,
+        onDetect: logic.onQrCodeDected),
+  );
+}
+
+Widget _showStepIndicating(HomePageState state) {
+  return Column(
+    children: [
+      Row(
+        children: [
+          Text('1. scan the first qrCode'),
+          Icon(Icons.done),
+        ],
+      ),
+      _wifiState(state),
+      Row(
+        children: [
+          Text('2. scan the second qrCode'),
+          Icon(Icons.done),
+        ],
+      ),
+      ElevatedButton(onPressed: null, child: Text('open gallery'))
+    ],
+  );
+}
+
+Widget _showImgSrc(HomePageLogic logic) {
+  return Row(
+    children: [
+      ElevatedButton(
+          onPressed: logic.pickImgWithCam, child: Text('using system camera')),
+      ElevatedButton(
+          onPressed: logic.pickImgFromGallery,
+          child: Text('choose from gallery'))
+    ],
+  );
+}
+
+Widget _wifiState(HomePageState state) {
+  return Column(
+    children: [
+      Text('wifi ${state.wifiConfig.wifiName} scanned,'),
+      Row(
+        children: [
+          Text('password: ${state.wifiConfig.wifiPwd}'),
+          Icon(Icons.copy)
+        ],
+      )
+    ],
+  );
 }
---
+++ lib/ui/home_page/home_page_state.dart
@@ -0,0 +1,12 @@
+import 'package:image_picker/image_picker.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:switch_screenshot_transfer/model/wifi_info.dart';
+
+class HomePageState {
+  MobileScannerController cameraController = MobileScannerController();
+
+  WifiInfo wifiConfig = WifiInfo();
+  
+  //TODO destruction , check doc
+  final ImagePicker picker = ImagePicker();
+}
